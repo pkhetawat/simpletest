071fdf00f29e5f63017ab9ace55a8d6564b8a109
pkhetawat <97152843+pkhetawat@users.noreply.github.com>
2023-08-31 21:45:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,22 +22,18 @@ apt-get install -y python3-pip
 apt-get install -y python3-devel
 apt-get install numactl
 apt-get install libffi-devel
-cd /root
+cd /usr/test-scripts
 wget -c https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
+python3 get-pip.py
 #python install ibm_db
 pip3 install ibm_db_sa
 pip3 install ibm_db
-pip install -U pyopenssl
-pip3 install --upgrade pip
-pip install --upgrade pip
-pip install --upgrade pip setuptools wheel
+#pip install -U pyopenssl
+#pip3 install --upgrade pip
+#pip install --upgrade pip
+#pip install --upgrade pip setuptools wheel
 
-wget -c https://nodejs.org/dist/v16.6.1/node-v16.6.1-linux-x64.tar.xz
-unxz node-v16.6.1-linux-x64.tar.xz 
-tar -xf node-v16.6.1-linux-x64.tar 
-mv node-v16.6.1-linux-x64 /root/nodejs 
-cd /root
-go get -d github.com/ibmdb/go_ibm_db 
-cd /root/go/src/github.com/ibmdb/go_ibm_db/installer 
-go run setup.go
+wget -c https://nodejs.org/dist/v20.5.1/node-v20.5.1-linux-x64.tar.xz
+unxz node-v20.5.1-linux-x64.tar.xz 
+tar -xf node-v20.5.1-linux-x64.tar
+mv node-v20.5.1-linux-x64 nodejs
